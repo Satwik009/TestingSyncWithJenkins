536c09256a5e6b051497ccf6885f02222c12a229
pankajg <pankajg@brickredsys.com>
2020-09-13 20:50:50 +0530

    testing jenkins sync

---
+++ src/test/java/TestCases/App1/TestOne.java
@@ -0,0 +1,17 @@
+package TestCases.App1;
+
+import org.testng.annotations.Test;
+
+public class TestOne {
+
+
+    @Test
+    public void test()
+    {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("Congratulations Satwik : Jenkins Syncing is Done");
+        }
+    }
+
+}
---
+++ src/test/java/TestCases/App2/Test1.java
@@ -0,0 +1,4 @@
+package TestCases.App2;
+
+public class Test1 {
+}
